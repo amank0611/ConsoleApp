b312968420f26210ec0b430f6b4c74444126c366
amank0611 <aman.kumar@smartdatainc.net>
2024-02-11 21:13:14 +0530

    added functionality to caclulate discount

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //input for billAmount
+            Console.WriteLine("Enter total bill amount");
+            long.TryParse(Console.ReadLine(), out long billAmount);
+            long result = CalculateDiscount(billAmount);
+            Console.Write($"final calculated discount is : {result}");
+            Console.ReadKey();
+        }
+        public static int CalculateDiscount(long billAmount)
+        {
+            // Write your code here example 136, 86, 357, 12294
+            int discount = 0;
+            long billAmt = billAmount;
+            while (billAmt > 0)
+            {
+                var digit = billAmt % 10;
+                if (digit % 2 != 0)
+                {
+                    discount += (int)digit;
+                }
+                billAmt /= 10;
+            }
+            return discount;
         }
     }
 }
